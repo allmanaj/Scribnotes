3d704a27f5c097a931acae84d7c22d626e0a6b04
Angus Allman <allmanaj@users.noreply.github.com>
2021-01-18 13:26:35 +0000

    Update settings.py

--- Scribnotes/settings.py
+++ Scribnotes/settings.py
@@ -133,5 +133,5 @@ CKEDITOR_CONFIGS = {
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
-ALLOWED_HOSTS = ['scribnotesapp.herokuapp.com', 'localhost', '127.0.0.1', 'scribbydoodoo.com', 'new-site123.com', 'scribnewtesting123456.pyhost.io']
+ALLOWED_HOSTS = ['scribnotesapp.herokuapp.com', 'localhost', '127.0.0.1',]
 #ALLOWED_HOSTS = []
